06a5ec8ef50bbf8357a6e9615313f7d38ddbae6b
Ivan Manan <ivanmanan@gmail.com>
2018-08-05 23:43:14 -0700

    Swapped bcrypt library because malfunctions in nodejs v10

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const debug = require('./debug');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 /////////////////////////////////////////////////////////////////////
 // Configurations
